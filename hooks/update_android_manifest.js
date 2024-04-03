90a6d0da93947197e15cac83d275a692a509e1c9
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-04-03 12:14:37 +0100

    testing dynamic queries

---
+++ hooks/update_android_manifest.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function (context) {
+    const cordovaCommon = context.requireCordovaModule('cordova-common');
+    const androidPlatformDir = path.join(context.opts.projectRoot, 'platforms/android');
+    const manifestPath = path.join(androidPlatformDir, 'app/src/main/AndroidManifest.xml');
+
+    if (!fs.existsSync(manifestPath)) {
+        console.error('AndroidManifest.xml not found.');
+        return;
+    }
+
+    let queriesValue = context.opts.plugin.pluginInfo.getPreferences().CORDOVA_ANDROID_QUERIES;
+    if (!queriesValue) {
+        console.error('CORDOVA_ANDROID_QUERIES variable not provided.');
+        return;
+    }
+
+    fs.readFile(manifestPath, 'utf-8', (err, data) => {
+        if (err) throw err;
+
+        let updatedManifest = data.replace('</manifest>', '');
+
+        queriesValue.split(',').forEach(packageName => {
+            updatedManifest += `<queries><package android:name="${packageName}"/></queries>`;
+        });
+
+        updatedManifest += '</manifest>';
+
+        fs.writeFile(manifestPath, updatedManifest, (err) => {
+            if (err) throw err;
+        });
+    });
+};
