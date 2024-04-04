95d482330cfb19c5773c81be8105d043ac137524
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-04-04 17:27:26 +0100

    Error Handling and Consistent Formatting

--- src/ios/AppAvailability.m
+++ src/ios/AppAvailability.m
@@ -4,20 +4,27 @@
 @implementation AppAvailability
 
 - (void)checkAvailability:(CDVInvokedUrlCommand*)command {
-    
     CDVPluginResult* pluginResult = nil;
     
-    NSString* scheme = [command.arguments objectAtIndex:0];
+    NSArray* arguments = command.arguments;
+    NSString* scheme = nil;
+    
+    if ([arguments count] > 0 && [arguments[0] isKindOfClass:[NSString class]]) {
+        scheme = arguments[0];
+    } else {
+        // Invalid or missing argument
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid or missing URL scheme"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        return;
+    }
     
     if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:scheme]]) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:(true)];
-    }
-    else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:(false)];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:NO];
     }
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    
 }
 
 @end
