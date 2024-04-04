5de97b0a6e2bb5d39ddc0adf10928a9a7609cfa5
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-04-04 17:40:50 +0100

    Update AppAvailability.java

--- src/android/AppAvailability.java
+++ src/android/AppAvailability.java
@@ -11,51 +11,46 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageInfo;
 
 public class AppAvailability extends CordovaPlugin {
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if(action.equals("checkAvailability")) {
-            String uri = args.getString(0);
-            this.checkAvailability(uri, callbackContext);
+        if (action.equals("checkAvailability")) {
+            String packageName = args.getString(0);
+            this.checkAvailability(packageName, callbackContext);
             return true;
         }
         return false;
     }
-    
-    // Thanks to http://floresosvaldo.com/android-cordova-plugin-checking-if-an-app-exists
-    public PackageInfo getAppPackageInfo(String uri) {
-        Context ctx = this.cordova.getActivity().getApplicationContext();
-        final PackageManager pm = ctx.getPackageManager();
+
+    private void checkAvailability(String packageName, CallbackContext callbackContext) {
+        PackageInfo packageInfo = getAppPackageInfo(packageName);
+        if (packageInfo != null) {
+            try {
+                JSONObject json = convertPackageInfoToJson(packageInfo);
+                callbackContext.success(json);
+            } catch (JSONException e) {
+                callbackContext.error("Error converting package info to JSON");
+            }
+        } else {
+            callbackContext.error("App not found");
+        }
+    }
+
+    private PackageInfo getAppPackageInfo(String packageName) {
+        Context context = cordova.getActivity().getApplicationContext();
+        PackageManager packageManager = context.getPackageManager();
 
         try {
-            return pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
-        }
-        catch(PackageManager.NameNotFoundException e) {
+            return packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+        } catch (PackageManager.NameNotFoundException e) {
             return null;
         }
     }
-    
-    private void checkAvailability(String uri, CallbackContext callbackContext) {
 
-        PackageInfo info = getAppPackageInfo(uri);
-
-        if(info != null) {
-            try {
-                callbackContext.success(this.convertPackageInfoToJson(info));
-            } 
-            catch(JSONException e) {
-                callbackContext.error("");    
-            }
-        }
-        else {
-            callbackContext.error("");
-        }
-    }
-
-    private JSONObject convertPackageInfoToJson(PackageInfo info) throws JSONException {
+    private JSONObject convertPackageInfoToJson(PackageInfo packageInfo) throws JSONException {
         JSONObject json = new JSONObject();
-        json.put("version", info.versionName);
-        json.put("appId", info.packageName);
-
+        json.put("version", packageInfo.versionName);
+        json.put("appId", packageInfo.packageName);
         return json;
     }
 }
