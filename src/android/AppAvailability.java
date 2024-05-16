48be2a2b672d4f64f9f207d947a1d7215dd4238b
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-05-16 16:58:00 +0100

    Update AppAvailability.java

--- src/android/AppAvailability.java
+++ src/android/AppAvailability.java
@@ -7,55 +7,68 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 
 public class AppAvailability extends CordovaPlugin {
+    
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if(action.equals("checkAvailability")) {
+        if ("checkAvailability".equals(action)) {
             String uri = args.getString(0);
-            this.checkAvailability(uri, callbackContext);
+            checkAvailability(uri, callbackContext);
             return true;
         }
         return false;
     }
-    
-    // Thanks to http://floresosvaldo.com/android-cordova-plugin-checking-if-an-app-exists
-    public PackageInfo getAppPackageInfo(String uri) {
-        Context ctx = this.cordova.getActivity().getApplicationContext();
-        final PackageManager pm = ctx.getPackageManager();
 
+    /**
+     * Get the PackageInfo of the app with the specified URI.
+     * 
+     * @param uri the URI scheme of the app
+     * @return PackageInfo of the app if found, null otherwise
+     */
+    private PackageInfo getAppPackageInfo(String uri) {
+        Context context = this.cordova.getActivity().getApplicationContext();
+        PackageManager pm = context.getPackageManager();
         try {
             return pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
-        }
-        catch(PackageManager.NameNotFoundException e) {
+        } catch (NameNotFoundException e) {
             return null;
         }
     }
-    
+
+    /**
+     * Check the availability of the app with the specified URI.
+     * 
+     * @param uri the URI scheme of the app
+     * @param callbackContext the callback context to send the result
+     */
     private void checkAvailability(String uri, CallbackContext callbackContext) {
-
         PackageInfo info = getAppPackageInfo(uri);
-
-        if(info != null) {
+        if (info != null) {
             try {
-                callbackContext.success(this.convertPackageInfoToJson(info));
-            } 
-            catch(JSONException e) {
-                callbackContext.error("");    
+                callbackContext.success(convertPackageInfoToJson(info));
+            } catch (JSONException e) {
+                callbackContext.error("Failed to convert package info to JSON.");
             }
-        }
-        else {
-            callbackContext.error("");
+        } else {
+            callbackContext.error("App not found.");
         }
     }
 
+    /**
+     * Convert PackageInfo to a JSONObject.
+     * 
+     * @param info the PackageInfo to convert
+     * @return JSONObject representing the PackageInfo
+     * @throws JSONException if JSON conversion fails
+     */
     private JSONObject convertPackageInfoToJson(PackageInfo info) throws JSONException {
         JSONObject json = new JSONObject();
         json.put("version", info.versionName);
         json.put("appId", info.packageName);
-
         return json;
     }
 }
