fdde690b749ec701dcdf670904f383a69ce22f0d
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-05-16 16:19:25 +0100

    Update AppAvailability.h

--- src/ios/AppAvailability.h
+++ src/ios/AppAvailability.h
@@ -1,21 +1,7 @@
-#ifndef AppAvailability_h
-#define AppAvailability_h
-
 #import <Cordova/CDV.h>
 
-/**
- * AppAvailability Cordova plugin for checking if an app is available.
- */
 @interface AppAvailability : CDVPlugin
 
-/**
- * Checks the availability of the specified app.
- *
- * @param command The command sent from JavaScript.
- *                Expects a string argument representing the URL scheme of the app to check.
- */
 - (void)checkAvailability:(CDVInvokedUrlCommand*)command;
 
 @end
-
-#endif /* AppAvailability_h */
