b544bbbe92602f50238894683967eb3d45c42f98
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-05-16 17:01:08 +0100

    Update AppAvailability.m

--- src/ios/AppAvailability.m
+++ src/ios/AppAvailability.m
@@ -3,21 +3,27 @@
 
 @implementation AppAvailability
 
+/**
+ * Check the availability of an app by its URL scheme.
+ *
+ * @param command The CDVInvokedUrlCommand object containing the URL scheme to check.
+ */
 - (void)checkAvailability:(CDVInvokedUrlCommand*)command {
-    
     CDVPluginResult* pluginResult = nil;
-    
     NSString* scheme = [command.arguments objectAtIndex:0];
     
-    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:scheme]]) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:(true)];
-    }
-    else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:(false)];
+    if (scheme != nil && [scheme length] > 0) {
+        NSURL *url = [NSURL URLWithString:scheme];
+        if ([[UIApplication sharedApplication] canOpenURL:url]) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"App not found."];
+        }
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid URL scheme."];
     }
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    
 }
 
 @end
