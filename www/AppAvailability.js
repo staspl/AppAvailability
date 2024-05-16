f4efb987a5cd751d4d9866690aa26af9af7d4bfb
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-05-16 16:37:42 +0100

    Update AppAvailability.js

--- www/AppAvailability.js
+++ www/AppAvailability.js
@@ -1,28 +1,38 @@
+/**
+ * Removes the trailing and leading whitespace on a URL scheme and verifies it's not empty.
+ * 
+ * @param {String} urlScheme the URL scheme to check.
+ * @param {Function} errorCallback to call when the urlScheme is empty.
+ * 
+ * @returns {String} urlScheme stripped by leading and trailing whitespace or undefined when it was empty.
+ */
 function checkUrlScheme(urlScheme, errorCallback) {
-    console.log("Checking URL scheme:", urlScheme); // Log the URL scheme being checked
     var trimmedUrlScheme = (urlScheme || "").trim();
     if (!trimmedUrlScheme) {
-        console.error("Invalid URL scheme:", urlScheme); // Log an error if the URL scheme is invalid
         errorCallback("Invalid URL scheme (\"" + urlScheme + "\"). Please ensure it's not empty or contains only whitespace.");
         return;
     }
-    console.log("Validated URL scheme:", trimmedUrlScheme); // Log the validated URL scheme
     return trimmedUrlScheme;
 }
 
 var appAvailability = {
 
+    /**
+     * Checks the availability of an app based on its URL scheme.
+     * 
+     * @param {String} urlScheme The URL scheme of the app to check.
+     * @param {Function} successCallback A callback function to call upon successful retrieval of app information.
+     * @param {Function} errorCallback A callback function to call upon failure or when no app is found.
+     */
     check: function(urlScheme, successCallback, errorCallback) {
-        console.log("Calling check function with URL scheme:", urlScheme); // Log the call to the check function
         urlScheme = checkUrlScheme(urlScheme, errorCallback);
+        // Only call the native plugin if we have a valid urlScheme
         if (urlScheme) {
-            console.log("Executing native plugin with URL scheme:", urlScheme); // Log the execution of the native plugin
             cordova.exec(successCallback, errorCallback, "AppAvailability", "checkAvailability", [urlScheme]);
-        } else {
-            console.error("URL scheme validation failed."); // Log an error if the URL scheme validation fails
         }
     }
     
 };
 
+// Expose the appAvailability object globally so it can be accessed from other scripts
 window.appAvailability = appAvailability;
