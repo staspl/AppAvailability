0fb57fb8696094ab9ed8503c3a76fff47a908e5d
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-05-17 11:59:12 +0100

    remove logs

--- www/AppAvailability.js
+++ www/AppAvailability.js
@@ -14,24 +14,19 @@ class AppAvailability {
      * @param {Function} onFail      - Callback function to be called on failure.
      */
     check(packageName, onSuccess, onFail) {
-        console.log(`AppAvailability.check called with packageName: ${packageName}`);
         if (typeof packageName !== "string") {
-            console.error("packageName must be a string");
             return onFail(new Error("packageName must be a string"));
         }
         
         if (!packageName.trim()) {
-            console.error("packageName cannot be empty");
             return onFail(new Error("packageName cannot be empty"));
         }
 
         cordova.exec(
             result => {
-                console.log(`AppAvailability.check success callback called with result: ${result}`);
                 onSuccess(result === 1);
             },
             error => {
-                console.error(`AppAvailability.check error callback called with error: ${error}`);
                 onFail(error);
             },
             "AppAvailability",
