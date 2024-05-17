c82fedb6d8d77248a9c73203ae8694c97535a761
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-05-17 11:17:44 +0100

    Update AppAvailability.java

--- src/android/AppAvailability.java
+++ src/android/AppAvailability.java
@@ -1,19 +1,22 @@
 package com.kelter.appavailability;
 
-import android.content.pm.PackageInfo;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class AppAvailability extends CordovaPlugin {
 
+    private static final String ACTION_CHECK_AVAILABILITY = "checkAvailability";
+
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        if ("checkAvailability".equals(action)) {
+        if (ACTION_CHECK_AVAILABILITY.equals(action)) {
             try {
                 final String packageName = args.getString(0);
                 cordova.getThreadPool().execute(new Runnable() {
@@ -32,20 +35,24 @@ public class AppAvailability extends CordovaPlugin {
 
     private void checkAvailability(String packageName, CallbackContext callbackContext) {
         try {
-            PackageInfo info = getPackageInfoCompat(packageName);
+            ApplicationInfo info = getApplicationInfoCompat(packageName);
             boolean isInstalled = (info != null);
-            callbackContext.success(isInstalled ? 1 : 0);
+            PluginResult result = new PluginResult(PluginResult.Status.OK, isInstalled ? 1 : 0);
+            callbackContext.sendPluginResult(result);
+        } catch (PackageManager.NameNotFoundException e) {
+            PluginResult result = new PluginResult(PluginResult.Status.OK, 0);
+            callbackContext.sendPluginResult(result);
         } catch (Exception e) {
             callbackContext.error("Error checking availability: " + e.getMessage());
         }
     }
 
-    private PackageInfo getPackageInfoCompat(String packageName) throws PackageManager.NameNotFoundException {
+    private ApplicationInfo getApplicationInfoCompat(String packageName) throws PackageManager.NameNotFoundException {
         PackageManager packageManager = this.cordova.getActivity().getPackageManager();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            return packageManager.getPackageInfo(packageName, PackageManager.PackageInfoFlags.of(0));
+            return packageManager.getApplicationInfo(packageName, PackageManager.ApplicationInfoFlags.of(0));
         } else {
-            return packageManager.getPackageInfo(packageName, 0);
+            return packageManager.getApplicationInfo(packageName, 0);
         }
     }
 }
