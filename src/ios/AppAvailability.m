f29da9e3b558ec93b57e23a2b6cf51427494b314
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-05-17 09:59:12 +0100

    updated ios code

--- src/ios/AppAvailability.m
+++ src/ios/AppAvailability.m
@@ -13,11 +13,12 @@
     NSString* scheme = [command.arguments objectAtIndex:0];
     
     if (scheme != nil && [scheme length] > 0) {
-        NSURL *url = [NSURL URLWithString:scheme];
+        NSString *urlString = [scheme hasPrefix:@"://"] ? scheme : [NSString stringWithFormat:@"%@://", scheme];
+        NSURL *url = [NSURL URLWithString:urlString];
         if ([[UIApplication sharedApplication] canOpenURL:url]) {
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
         } else {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"App not found."];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:NO];
         }
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid URL scheme."];
